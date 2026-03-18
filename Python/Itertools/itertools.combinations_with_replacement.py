2afb7a0eee9f471e18f01a25f38a025e750f9002
Aryan Chande <aryanchande06@gmail.com>
2026-03-18 11:40:29 +0530

    Solutions

---
+++ Python/Itertools/itertools.combinations_with_replacement.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import combinations_with_replacement
+s,n=input().split()
+n=int(n)
+s=sorted(s)
+for j in combinations_with_replacement(s,n):
+    print("".join(j))
+
