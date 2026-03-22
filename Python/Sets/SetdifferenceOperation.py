3874b84a0cb077620d027136d3d03ed3fc254f0d
Aryan Chande <aryanchande06@gmail.com>
2026-03-22 18:53:53 +0530

    Solutions

---
+++ Python/Sets/SetdifferenceOperation.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+en=int(input())
+ren=set(map(int,input().split()))
+fn=int(input())
+rfn=set(map(int,input().split()))
+s=ren-rfn
+print(len(s))
