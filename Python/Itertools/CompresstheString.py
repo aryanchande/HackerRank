24dfa400d7ff7ca4665c188a40f6266bd9060311
Aryan Chande <aryanchande06@gmail.com>
2026-08-16 19:25:33 +0530

    solutions

---
+++ Python/Itertools/CompresstheString.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import groupby
+s=input()
+for key,group in groupby(s):
+    print(f"({len(list(group))}, {int(key)})",end=" ")
+
